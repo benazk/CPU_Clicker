e5bb1136032cfb42d8e77b9e64f0e964955ee4e5
benazk <benatezquerro128@gmail.com>
2024-04-26 13:32:58 +0200

    cambios en Juego.java

--- Juego/src/Info.java
+++ Juego/src/Info.java
@@ -2,9 +2,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRootPane;
 
 public class Info extends JFrame implements ActionListener {
+	public static JLabel lblBits = new JLabel("1");
+	
+	public static Info info;
+	
+	Info(){
+		
+		setTitle("CPU Clicker");
 
+	    setSize(500, 300);
+
+	    setUndecorated(true);
+	    
+		getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+
+	    setLayout(null);
+	    
+	    setLocation(533,268);
+	    
+		lblBits.setLocation(0,0);
+		lblBits.setSize(100,20);
+		add(lblBits);
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
@@ -12,7 +37,8 @@ public class Info extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		info = new Info();
+		info.setVisible(true);
 
 	}
 
