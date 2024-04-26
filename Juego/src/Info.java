d7a22344e5f359ac3b6824009389c22161713624
benazk <benatezquerro128@gmail.com>
2024-04-26 11:25:25 +0200

    añadido proyecto de java

---
+++ Juego/src/Info.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class Info extends JFrame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
